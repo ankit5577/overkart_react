5f4d630c6af6355632689e002b34b823ae5b3e66
Ankit Kaushal <kaushalankit101010@gmail.com>
2021-12-05 19:24:32 +0530

    button changes

--- Frontend/src/components/BottomNav.js
+++ Frontend/src/components/BottomNav.js
@@ -23,10 +23,18 @@ function BottomNav() {
         </Button>
       </div>
       <div className="flex flex-1">
-        <Button class="flex flex-grow">Checkign</Button>
+        <Button class="flex flex-grow"><img
+            className="self-center mx-auto"
+            src="https://img.icons8.com/material-outlined/24/000000/user-male-circle.png"
+            alt="profile"
+          /></Button>
       </div>
       <div className="flex flex-1">
-        <Button class="flex flex-grow">Checkign</Button>
+        <Button class="flex flex-grow"><img
+            className="self-center mx-auto"
+            src="https://img.icons8.com/material-outlined/24/000000/home--v2.png"
+            alt="home"
+          /></Button>
       </div>
     </nav>
   );
